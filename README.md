8e80162a4fadb9178261bbba1fcc8572d80eb483
Nikolay Nechaev <Nikolay_Nechaev@mail.ru>
2022-03-21 23:09:39 +0300

    Initial commit
    Already contains all the code: moved from the "some_scripts_and_configs"
    repository

---
+++ README.md
@@ -0,0 +1,28 @@
+# CPU energy preference switcher (plus)
+
+This tool lets you switch performance preferences for a CPU on Linux via cpufreq driver (`cpufreq-utils` is not needed). Features:
+
+-  Get current modes for CPU cores
+-  Set new mode for all cores
+-  Switch to "extreme powersave mode": force the lowest possible frequency for all cores
+
+## Preparation
+
+To use the script, you should have write permissions on a couple of files inside /sys. You can either always run the script as root, or create a group and give it appropriate permissions. For the second:
+
+1.  Create a new group: `groupadd cpu_tuners`
+2.  Add user(s) to it: `usermod -aG cpu_tuners $USER`
+3.  Create a systemd service to give permissions:
+    -  Put the service file to /etc/systemd/system/
+    -  Run `systemctl enable --now update_sysfs_cpu_permissions.service` (you need that because the permissions of virtual files are reset on every reboot)
+
+## Usage
+
+Just run the script! Examples:
+```
+./cpu_energy_preference_switcher.sh get          # View current preferences
+./cpu_energy_preference_switchher.sh list        # List valid options for `set` (for the first core)
+./cpu_energy_preference_switcher.sh set default  # Set a mode for all cores
+./cpu_energy_preference_switcher.sh extreme      # Enter extreme powersave mode (force lowest possible frequency)
+./cpu_energy_preference_switcher.sh unextreme    # Exit extreme powersave mode
+```
---
+++ cpu_energy_preference_switcher_plus.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# The name of this file is quite long and hard to type quickly. When putting to my machine,
+# I typically rename it to something shorter, e.g. just "cpu"
+
+
+GLOBAL_DIR="/sys/devices/system/cpu/cpufreq/"
+
+
+# FIXME: Help message looks bad when printed out
+HELP_MESSAGE="This utility allows you to switch CPU energy performance preference.
+\n\n
+It interacts with the files located in the directories /sys/devices/system/cpu/cpufreq/policy\*
+\n\n\n
+The following usages are available:
+\n\n
+$0 help - display this help message and exit
+\n\n
+$0 list - get available performance preferences (print the energy_performance_available_preferences file of policy0)
+\n\n
+$0 get_current - get currently selected performance preferences of all policies (contents of the
+energy_performance_preference files)
+\n\n
+$0 get - synonym of get_current
+\n\n
+$0 set <performance_mode> - write <performance_mode> to the energy_performance_preference files of all policies
+\n\n
+$0 extreme - (extreme powersave mode) force constant lowest possible frequency: set frequency upper limit to the
+lowest value alowed by hardware (copy contents of cpuinfo_min_freq to file scaling_max_freq for each policy)
+\n\n
+$0 unextreme - reset frequency upper limit (back?) to the highest value allowed by hardware (copy contents of
+cpuinfo_max_freq to file scaling_max_freq for each policy)
+"
+
+# WARNING: there must be write permissions for the
+# `/sys/devices/system/cpu/cpufreq/policy*/{energy_performance_preference,scaling_max_freq}` files for this script in
+# order to work correctly
+
+case "$1" in
+	""|help|-h|--help)
+		exec echo -e $HELP_MESSAGE
+		;;
+	list)
+		exec cat $GLOBAL_DIR/policy0/energy_performance_available_preferences
+		;;
+	get_current|get)
+		# Using `tail -n +0` instead of `cat` to have filenames printed
+		exec tail -n +0 $GLOBAL_DIR/policy*/energy_performance_preference
+		;;
+	set)
+		echo $2 | tee "$GLOBAL_DIR"/policy*/energy_performance_preference
+		exit $?
+		;;
+	extreme)
+		for policy in $GLOBAL_DIR/policy*; do
+			# Forcefully disallow any freequency changes
+			cat $policy/cpuinfo_min_freq > $policy/scaling_max_freq
+		done
+		;;
+	unextreme)
+		for policy in $GLOBAL_DIR/policy*; do
+			cat $policy/cpuinfo_max_freq > $policy/scaling_max_freq
+		done
+		;;
+	*)
+		echo 'Error: unknown command. See help (run `$0 help)' 1>&2
+		exit 101
+		;;
+esac
